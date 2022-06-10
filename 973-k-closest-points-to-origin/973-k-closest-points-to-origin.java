59226fd8268f9a363a31f3bae722d91741781142
vishalsachan5881 <68497794+vishalsachan5881@users.noreply.github.com>
2022-06-10 12:22:11 +0530

    Time: 47 ms (47.32%), Space: 119 MB (9.63%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -8,20 +8,26 @@ class Pair{
 }
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Pair[] arr = new Pair[points.length];
-        for(int i = 0;i<points.length;i++)
-            arr[i] = new Pair(points[i][0],points[i][1]);
+//         Pair[] arr = new Pair[points.length];
+//         for(int i = 0;i<points.length;i++)
+//             arr[i] = new Pair(points[i][0],points[i][1]);
         
-        Arrays.sort(arr,(a,b)->{
-            return ((a.x*a.x+a.y*a.y)-(b.x*b.x+b.y*b.y));
+//         Arrays.sort(arr,(a,b)->{
+//             return ((a.x*a.x+a.y*a.y)-(b.x*b.x+b.y*b.y));
+//         });
+        
+        Arrays.sort(points,(a,b)->{
+            return ((a[0]*a[0]+a[1]*a[1]) - (b[0]*b[0]+b[1]*b[1])); 
         });
             
-        int[][] ans =  new int[k][2];
-        for(int i = 0;i<k;i++){
-            ans[i][0] = arr[i].x;
-              ans[i][1] = arr[i].y;
-        }
         
-        return ans;
+        return Arrays.copyOfRange(points,0,k);
+//         int[][] ans =  new int[k][2];
+//         for(int i = 0;i<k;i++){
+//             ans[i][0] = arr[i].x;
+//               ans[i][1] = arr[i].y;
+//         }
+        
+//         return ans;
     }
 }
