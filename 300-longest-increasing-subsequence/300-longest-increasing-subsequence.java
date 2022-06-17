6c089af4f3d1624ea6574025599d4b8af23228f5
vishalsachan5881 <68497794+vishalsachan5881@users.noreply.github.com>
2022-06-17 12:27:43 +0530

    Time: 14 ms (70.17%), Space: 45 MB (16.92%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,39 +1,24 @@
 class Solution {
-    public int fn(int nums[], int ind, int prev,int[][] dp){
-        if(ind == nums.length) return 0;
-        if(dp[ind][prev+1] != -1) return dp[ind][prev+1];
-        int l1 = fn(nums, ind+1, prev, dp);
-        if(prev == -1 || nums[ind] > nums[prev])
-           l1 = Math.max(l1 , 1+fn(nums, ind+1, ind, dp));
-      dp[ind][prev+1]=l1;
-        return l1;
+    public int bound(LinkedList<Integer> ls, int key){
+        int ind = Collections.binarySearch(ls, key);
+        if(ind < 0)
+            return Math.abs(ind)-1;
+        else
+            return ind;
     }
     public int lengthOfLIS(int[] nums) {
-        int n  = nums.length;
-//         int[][] dp = new int[n+1][n+1];
-//         for(int ind  = n-1; ind>=0;ind--)
-//             for(int prev = ind-1 ; prev>=-1;prev--)
-//             {
-//                 int l1=  dp[ind+1][prev+1];
-//                    if(prev == -1 || nums[ind] > nums[prev]) 
-//                        l1  = Math.max(l1, 1+dp[ind+1][ind+1]);
-                
-//                 dp[ind][prev+1] = l1;
-//             }
-//     return dp[0][0];
-        int[] dp = new int[n];
-        Arrays.fill(dp,1);
-        for(int i = 0 ; i<n;i++){
-            for(int prev = 0;prev <i;prev++){
-                if(nums[i] > nums[prev])
-                 dp[i] = Math.max(dp[i],dp[prev]+1);
+        int n = nums.length;
+        LinkedList<Integer> ls = new LinkedList<>();
+        ls.addFirst(nums[0]);
+        for(int i = 1; i<n;i++)
+        {
+            if(ls.getLast() < nums[i])
+                ls.add(nums[i]);
+            else{
+                int ii = bound(ls, nums[i]);
+                ls.set(ii ,nums[i]);
             }
-        
         }
-        int max  = 0;
-        for(var v : dp)
-            max = Math.max(v, max);
-        
-        return max;
+        return ls.size();
     }
 }
