98052cd160ed322e72cab8e620cc36be22963ac9
vishalsachan5881 <68497794+vishalsachan5881@users.noreply.github.com>
2022-08-01 10:45:13 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (92.06%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,16 +1,14 @@
 class Solution {
-    public int fn(int m, int n,int[][] dp)
-    {
-        if(m==0 && n==0) return 1;
-        if(m<0 || n<0) return 0;
-        if(dp[m][n] != -1) return dp[m][n];
-        return dp[m][n] = (fn(m-1,n,dp)+fn(m,n-1,dp));
-    }
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        for(int i = 0;i<m;i++)
-            for(int j = 0; j<n;j++)
-                dp[i][j]= -1;
-        return fn(m-1,n-1,dp);
+        for(int i = m-1 ; i>=0;i--){
+            for(int j = n-1 ; j>=0;j--){
+                if(i==m-1 || j == n-1) dp[i][j] = 1;
+                else{
+                    dp[i][j] = dp[i+1][j]+dp[i][j+1];
+                }
+            }
+        }
+        return dp[0][0];
     }
 }
