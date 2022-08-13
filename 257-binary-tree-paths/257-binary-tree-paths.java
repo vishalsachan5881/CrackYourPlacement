f3cb9cdc22003d83e47e8e81b65dabc3b0bf893c
vishalsachan5881 <68497794+vishalsachan5881@users.noreply.github.com>
2022-08-14 02:32:26 +0530

    Time: 13 ms (45.00%), Space: 42.7 MB (87.28%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.java
+++ 257-binary-tree-paths/257-binary-tree-paths.java
@@ -14,29 +14,59 @@
  * }
  */
 class Solution {
-    public void fn(TreeNode root,String al,List<String>ans){
-        if(root.left==null&&root.right==null){
-            //al.append("->"+root.val);
-            al += ("->"+root.val);
-            //al.delete(0,1);
-            ans.add(al.substring(3));
+    public void fn(TreeNode root,List<Integer> al,List<List<Integer>>ans){
+         
+        if(root == null) return;
+        //System.out.println(al);
+        if(root.left == null && root.right == null){
+            al.add(root.val);
+           // System.out.println(al);
+            ans.add(new ArrayList<>(al));
+            al.remove(al.size()-1);
             return;
         }
         
-        if(root.left!=null){
-            fn(root.left,al+"->"+root.val,ans);
-        }
-        
-        if(root.right!=null){
-            fn(root.right,al+"->"+root.val,ans);
-        }
-       
+        al.add(root.val);
+        fn(root.left, al, ans);
+        fn(root.right, al, ans);
+        al.remove(al.size()-1);
     }
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> ans = new ArrayList<>();
-        String al = " ";
-        fn(root,al,ans);
+        List<Integer> al = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        fn(root, al, ans);
+        List<String> anss  = new ArrayList<>();
+        for(var v: ans){
+            String temp = "";
+            for(int i = 0 ; i<v.size();i++){
+                if(i!=v.size()-1) temp+=(v.get(i)+"->");
+                else temp+= v.get(i);
+            }
+            anss.add(temp);
+        }
         System.out.println(ans);
-        return ans;
-    }
+        return anss;
+    } 
 }
+
+
+// public List<String> binaryTreePaths(TreeNode root) {
+// 	List<String> res = new LinkedList<>();
+// 	backTracking(root, res, new ArrayList<>(), new StringBuilder());
+// 	return res;
+// }
+// private void backTracking(TreeNode node, List<String> collector, List<Integer> tmp, StringBuilder sb) {
+// 	if (node.left == null && node.right == null) {
+// 		for (int val : tmp) {
+// 			sb.append(val).append("->");
+// 		}
+// 		sb.append(node.val);
+// 		collector.add(sb.toString());
+// 		sb.setLength(0);
+// 		return;
+// 	}
+// 	tmp.add(node.val);
+// 	if (node.left != null) backTracking(node.left, collector, tmp, sb);
+// 	if (node.right != null) backTracking(node.right, collector, tmp, sb);
+// 	tmp.remove(tmp.size()-1);
+// }
