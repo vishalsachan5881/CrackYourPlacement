5c19a3723c91b84c242d94e7d70909fbea325400
vishalsachan5881 <68497794+vishalsachan5881@users.noreply.github.com>
2022-08-24 15:30:15 +0530

    Time: 899 ms (6.80%), Space: 69.6 MB (8.22%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,18 +1,27 @@
 class Solution {
-   public boolean canJump(int[] nums) {
-    int[] dp = new int[nums.length];
-    Arrays.fill(dp ,-1);
-    return helper(0, nums, dp);
-}
-
-public boolean helper(int index, int[] nums, int[] dp){
-    if(index == nums.length-1) return true;
-    if(dp[index] != -1) return dp[index] == 1;
-    for(int i=1; i<=nums[index]; i++){
-        boolean current = helper(index+i, nums, dp);
-        dp[index] = (current == true ? 1 : 0);
-        if(current == true) return true;
+    public boolean fn(int[] nums, int ind,Boolean[] hm){
+        
+         
+        if(ind == nums.length-1) return true;
+        if(ind >= nums.length || nums[ind] == 0) return false;
+        
+        if(hm[ind] != null)  return hm[ind];
+        boolean min = false;
+        for(int i = 1 ; i <= nums[ind];i++){
+             if(fn(nums, ind+i,hm))
+                 return hm[ind] = true;
+        }
+        return hm[ind] = min;
+        
     }
-    return false;
-}
+    public boolean canJump(int[] nums) {
+        //int ind = nums.length-1;
+         Boolean[] hm = new Boolean[nums.length+1];
+         if(nums.length==1) return true;
+         Arrays.fill(hm,null);
+         fn(nums,0,hm);
+         System.out.println(hm[0]);
+         if(hm[0] == null) return false;
+         return hm[0];
+    }
 }
