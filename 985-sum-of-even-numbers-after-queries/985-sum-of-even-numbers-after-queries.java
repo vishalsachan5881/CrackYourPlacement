c31438b3ee63a90820c4db0f90f8c93135ee567a
vishalsachan5881 <68497794+vishalsachan5881@users.noreply.github.com>
2022-09-21 12:59:45 -0700

    Time: 7 ms (60.71%), Space: 69.1 MB (69.80%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.java
@@ -1,37 +1,20 @@
 class Solution {
-    public void print(int[] nums){
-        for(var v : nums)
-            System.out.print(v+" ");
-        
-        System.out.println();
-    }
-    public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
-        int even = 0, odd = 0;
-        for(var v : nums)
-            if(v%2 == 0) even+=v;
-            else odd+=v;
-        
-        int n = queries.length, j = 0 ;
-        int[] ans = new int[n];
-        for(int i = 0 ; i < n;i++){
-            int val = queries[i][0];
-            int index = queries[i][1];
-            
-            if(val%2 != 0 && nums[index]%2!=0){
-                even = even + nums[index] + val;
-                
-            }
-            else if (val % 2 !=0 && nums[index] % 2 == 0 ){
-                even -= nums[index];
-               
-            }
-            else if(val % 2 ==0 && nums[index]%2 == 0){
-                even += val;
-                
-            }
-            ans[j++] = even;
-                nums[index] += val;
+    public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
+        int S = 0;
+        for (int x: A)
+            if (x % 2 == 0)
+                S += x;
+
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; ++i) {
+            int val = queries[i][0], index = queries[i][1];
+            if (A[index] % 2 == 0) S -= A[index];
+            A[index] += val;
+            if (A[index] % 2 == 0) S += A[index];
+            ans[i] = S;
         }
+
         return ans;
     }
 }
