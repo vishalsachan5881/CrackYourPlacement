c2dfbb8c3e06545f25097e08427f69bf273e562f
vishalsachan5881 <68497794+vishalsachan5881@users.noreply.github.com>
2022-10-05 00:19:23 -0700

    Time: 4 ms (51.56%), Space: 43.3 MB (30.03%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,61 +1,62 @@
 class Pair{
-    int row, col;
-    public Pair(int row, int col){
-        this.row = row;
-        this.col = col;
+    int x, y;
+    public Pair(int x, int y){
+        this.x = x;
+        this.y = y;
     }
 }
 class Solution {
-    public boolean isValid(int row, int col , int[][] grid){
-        int rown = grid.length;
-        int coln = grid[0].length;
-        
-        return (row >= 0 && col >= 0 && row < rown && col < coln && grid[row][col] == 1) ;
+    static int rowc;
+    static int colc;
+    
+    
+    public boolean isValid(int row, int col, int[][] grid){
+        if(row >= rowc || col >= colc || row < 0 || col < 0 || grid[row][col] != 1)
+            return false;
+        return true;
     }
-    public int orangesRotting(int[][] grid) {
-        int rowc  = grid.length;
-         int colc = grid[0].length;
-        int total = 0;
-        Queue<Pair> q = new LinkedList<>();
         
-        for(int i = 0 ; i<rowc;i++){
-            for(int j = 0; j<colc;j++){
-                if(grid[i][j] != 0) total++;
-                if(grid[i][j] == 2) q.offer(new Pair(i,j));
-            }
-        }
-        // Queue<>
-        if(total == 0 ) return 0;
-        int t = 0;
+        
+    public int orangesRotting(int[][] grid) {
+         rowc= grid.length;
+         colc= grid[0].length;
+        int count = -1, orange = 0 ;
+        Queue<Pair> q = new LinkedList<>();
+        for(int i = 0 ; i < rowc ; i++)
+            for(int j = 0 ; j < colc; j++)
+                {if(grid[i][j] == 2)
+                    q.offer(new Pair(i,j));
+                 if(grid[i][j] == 1) orange++;
+                }
+        if(orange == 0) return 0;
         while(!q.isEmpty()){
-            t++;
-            int size = q.size();
-            for(int i = 0 ; i < size;i++){
-                Pair p = q.poll();
-                
-                if(p.row + 1 < rowc && grid[p.row+1][p.col] == 1){
-                    grid[p.row+1][p.col] = 2;
-                    q.offer(new Pair(p.row+1, p.col));
+            int n = q.size();
+            for(int i = 0 ; i  < n ; i++){
+                Pair curr = q.poll();
+                if(isValid(curr.x+1, curr.y,grid)) {
+                    grid[curr.x+1][curr.y] = 2;
+                    q.offer(new Pair(curr.x+1, curr.y));
                 }
-                 if(p.row -1 >=0 && grid[p.row-1][p.col] == 1){
-                    grid[p.row-1][p.col] = 2;
-                    q.offer(new Pair(p.row-1, p.col));
+                if(isValid(curr.x-1, curr.y,grid)) {
+                    grid[curr.x-1][curr.y] = 2;
+                    q.offer(new Pair(curr.x-1, curr.y));
                 }
-                 if(p.col + 1 < colc && grid[p.row][p.col+1] == 1){
-                    grid[p.row][p.col+1] = 2;
-                    q.offer(new Pair(p.row, p.col+1));
+                if(isValid(curr.x, curr.y+1,grid)) {
+                    grid[curr.x][curr.y+1] = 2;
+                    q.offer(new Pair(curr.x, curr.y+1));
                 }
-                 if(p.col - 1>=0 && grid[p.row][p.col-1] == 1){
-                    grid[p.row][p.col-1] = 2;
-                    q.offer(new Pair(p.row, p.col-1));
+                if(isValid(curr.x, curr.y-1,grid)) {
+                    grid[curr.x][curr.y-1] = 2;
+                    q.offer(new Pair(curr.x, curr.y-1));
                 }
-                
             }
+            count++;
         }
-        for(int  i = 0 ; i < rowc;i++)
-            for(int j = 0 ; j<colc;j++)
-        {        if(grid[i][j] == 1) return -1;
-        }
-            return t-1;
+        
+        for(int i  = 0 ; i < rowc;i++)
+            for(int j = 0; j < colc; j++)
+                if(grid[i][j] == 1) return -1;
+              
+        return count;
     }
 }
