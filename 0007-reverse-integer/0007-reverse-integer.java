a9910b00d3f2a052667eec58a2435ee7a0d39d48
vishalsachan5881 <68497794+vishalsachan5881@users.noreply.github.com>
2023-07-12 00:57:34 -0700

    Time: 1 ms (100.00%), Space: 39.8 MB (71.81%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+        long sum=0;
+        int k=x;
+        if(x<0)
+        k=(-1)*k;
+        while(k>0){
+        sum=sum*10+x%10;
+        x=x/10;
+        k=k/10;
+        }
+        if(sum<Math.pow(-2,31) || sum>Math.pow(2,31) )
+            return 0;
+        else 
+            return (int)sum;
+    }
+}
