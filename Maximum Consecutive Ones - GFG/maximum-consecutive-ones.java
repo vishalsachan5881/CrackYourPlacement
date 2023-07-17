11808a5726718bb7252355f86b10326d679131f4
vishalsachan5881 <68497794+vishalsachan5881@users.noreply.github.com>
2023-07-17 02:11:36 -0700

    Added solution - LeetHub

---
+++ Maximum Consecutive Ones - GFG/maximum-consecutive-ones.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] nums = IntArray.input(br, n);
+            
+            
+            int k;
+            k = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            int res = obj.longestOnes(n, nums, k);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int longestOnes(int n, int[] arr, int k) {
+        // code here
+        int i = 0, j = 0, zc =  0, ans = 0;
+        while(j<n){
+            if(arr[j] == 0)
+            zc++;
+            
+            if(zc>k){
+                while(zc>k){
+                    if(arr[i] == 0){
+                        zc--;
+                    }
+                    i++;
+                }
+            }
+            
+            ans = Math.max(ans, j-i+1);
+            j++;
+        }
+        return ans;
+    }
+}
+        
