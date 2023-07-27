8868d8b76d6a10b095a01254f6ff13890c9c8d83
vishalsachan5881 <68497794+vishalsachan5881@users.noreply.github.com>
2023-07-27 11:43:06 -0700

    Added solution - LeetHub

---
+++ Longest consecutive subsequence - GFG/longest-consecutive-subsequence.java
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+class Driverclass
+{
+    // Driver Code
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+    
+	public static void main(String args[])
+	{
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			
+			for(int i=0; i<n; i++)
+				a[i] = sc.nextInt();
+		    out.println(new Solution().findLongestConseqSubseq(a, n));
+		    t--;
+		}
+		out.close();
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{   
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+	static int findLongestConseqSubseq(int arr[], int N)
+	{
+	   // add your code here
+	   HashMap<Integer, Boolean> hm = new HashMap<>();
+	   for(var v : arr)
+	   hm.put(v, true);
+	   
+	   for(var v : hm.keySet()){
+	       if(hm.containsKey(v-1)){
+	           hm.put(v, false);
+	       }
+	   }
+	   int maxi = 0, count = 0;
+	   for(var v : hm.keySet()){
+	       if(hm.get(v)){
+	           for(int i = v ; hm.containsKey(i) && i <= 100000; i++){
+	               count++;
+	           }
+	       }
+	       maxi = Math.max(maxi, count);
+	       count = 0;
+	   }
+	   return maxi;
+	}
+}
